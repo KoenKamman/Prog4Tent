184ca539adf134c284e938ef6eafbcc82348003d
Whrabbit <whitneyrabbitc@gmail.com>
2017-06-13 12:32:37 +0200

    added loginactivity

--- Prog4Tent_Android/app/src/main/java/nl/code7/prog4tent_android/MainActivity.java
+++ Prog4Tent_Android/app/src/main/java/nl/code7/prog4tent_android/MainActivity.java
@@ -1,13 +1,18 @@
 package nl.code7.prog4tent_android;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import nl.code7.prog4tent_android.activity.LoginActivity;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+//        setContentView(R.layout.activity_main);
+        Intent i = new Intent(getApplicationContext(), LoginActivity.class);
+        startActivity(i);
     }
 }
