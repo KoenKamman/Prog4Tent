a344d2009ba7d3d2486e3a90b223bd54b3c63f7d
Zandkorrel <Zandkorrel@users.noreply.github.com>
2017-06-14 22:16:17 +0200

    Wrote tests for login.

--- Prog4Tent_Node/test/test.js
+++ Prog4Tent_Node/test/test.js
@@ -9,12 +9,46 @@ var should = chai.should();
 
 chai.use(chaiHttp);
 
-describe('Login Test', function() {
-    it('Test POST /api/v1/login SHOULD FAIL', function(done) {
+describe('Login Test', function () {
+    it('Empty Values POST /api/v1/login', function (done) {
         chai.request(server)
             .post('/api/v1/login')
-            .send({"username":"asdasd","password":"asdsad"})
-            .end(function(err, res) {
+            .send({"username": "", "password": ""})
+            .end(function (err, res) {
+                res.should.have.status(400);
+                res.body.should.have.property('error');
+                done();
+            });
+    });
+    it('Empty Body POST /api/v1/login', function (done) {
+        chai.request(server)
+            .post('/api/v1/login')
+            .end(function (err, res) {
+                res.should.have.status(400);
+                res.body.should.have.property('error');
+                done();
+            });
+    });
+
+    it('Correct Credentials POST /api/v1/login', function (done) {
+        chai.request(server)
+            .post('/api/v1/login')
+            .send({"username": "test", "password": "testing"})
+            .end(function (err, res) {
+                res.should.have.status(200);
+                res.body.should.have.property('username');
+                res.body.should.have.property('token');
+                done();
+            });
+    });
+});
+
+describe('Register Test', function () {
+    it('Empty Values POST /api/v1/login', function (done) {
+        chai.request(server)
+            .post('/api/v1/login')
+            .send({"username": "", "password": ""})
+            .end(function (err, res) {
                 res.should.have.status(400);
                 done();
             });
