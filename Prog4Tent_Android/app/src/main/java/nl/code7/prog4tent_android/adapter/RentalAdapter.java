d18b2b0a6adf373e7baecaedb3b6069a95527191
Zandkorrel <kammankoen@gmail.com>
2017-06-20 10:34:29 +0200

    Changed how dates are displayed.

--- Prog4Tent_Android/app/src/main/java/nl/code7/prog4tent_android/adapter/RentalAdapter.java
+++ Prog4Tent_Android/app/src/main/java/nl/code7/prog4tent_android/adapter/RentalAdapter.java
@@ -8,7 +8,11 @@ import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.TextView;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
 
 import nl.code7.prog4tent_android.R;
 import nl.code7.prog4tent_android.domain.Film;
@@ -21,6 +25,8 @@ import static nl.code7.prog4tent_android.R.string.film;
  */
 
 public class RentalAdapter extends ArrayAdapter<Rental> {
+    private String returnOn;
+
     public RentalAdapter(Context context, ArrayList<Rental> rentals){
         super(context, 0, rentals);
     }
@@ -28,7 +34,6 @@ public class RentalAdapter extends ArrayAdapter<Rental> {
     public View getView(int position, View convertview, ViewGroup parent) {
 
         Rental rental = getItem(position);
-        //Film film = null;
         if (convertview == null){
             convertview = LayoutInflater.from(getContext()).inflate(R.layout.listview_rental_item_row, parent, false);
         }
@@ -41,12 +46,24 @@ public class RentalAdapter extends ArrayAdapter<Rental> {
             rentalDate = rentalDate.substring(0, spaceIndex);
         }
 
+        try {
+            SimpleDateFormat parser = new SimpleDateFormat("yyyy-MM-dd");
+            Date date = parser.parse(rentalDate);
+            Calendar c = Calendar.getInstance();
+            c.setTime(date);
+            c.add(Calendar.DATE, Integer.parseInt(rental.getRental_duration()));
+            Date newDate = c.getTime();
+            returnOn = parser.format(newDate);
+        } catch (ParseException e){
+            e.printStackTrace();
+        }
+
         TextView filmName = (TextView) convertview.findViewById(R.id.rental_item_TextView);
         filmName.setText(rental.getTitle());
         TextView filmRentalDate = (TextView) convertview.findViewById(R.id.rental_date_TV);
         filmRentalDate.setText(rentalDate);
         TextView filmPeriod = (TextView) convertview.findViewById(R.id.rental_period_TV);
-        //filmPeriod.setText(film.getRental_duration());
+        filmPeriod.setText(returnOn);
 
 
         return convertview;
